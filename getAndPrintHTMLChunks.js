462e180b71a643e75be2b558b52491f15f08c4ec
digitalfabric92 <dwawryko@gmail.com>
2017-10-09 11:40:14 -0400

    Added step 1 - printing in chunks.

---
+++ getAndPrintHTMLChunks.js
@@ -0,0 +1,29 @@
+// while https is built-in to Node, it is a module, so it must be required.
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+    var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+    };
+
+    /* Add your code here */
+    // the callback is invoked when a `data` chunk is received
+
+https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+        console.log('Chunk Received. Length:', data.length);
+        console.log(data);
+        //httpsString += data;
+
+    });
+
+});
+
+}
+
+getAndPrintHTMLChunks();
