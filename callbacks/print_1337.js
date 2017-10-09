f6c470219a55f9ae7c95909d6fe32d83ee60f11e
digitalfabric92 <dwawryko@gmail.com>
2017-10-09 17:06:59 -0400

    Initial commit

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -1,1 +1,21 @@
 var getHTML = require('./http-functions');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step6/uppercase.html'
+};
+
+var map = {
+    'ck':'x',
+    'er' : '0r',
+    'you' : 'j00',
+    'a' : '4',
+    'e' : '3',
+    'l' : '1',
+    'o' : '0',
+    's' : '5',
+    't' : '7'
+};
+
+
+//var ss = s.toLowerCase().replace(/\s/g, '');
