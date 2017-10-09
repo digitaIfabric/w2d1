d38962fc4aebe6a1139f679761bf5f905cf4b2d5
digitalfabric92 <dwawryko@gmail.com>
2017-10-09 12:08:12 -0400

    Added step 3 - printing with Parameters

---
+++ getHTML.js
@@ -0,0 +1,19 @@
+var https = require('https');
+
+
+function getHTML (options, callback) {
+
+    /* Add your code here */
+
+}
+
+function printHTML (html) {
+    console.log(html);
+}
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+};
+
+getHTML();
